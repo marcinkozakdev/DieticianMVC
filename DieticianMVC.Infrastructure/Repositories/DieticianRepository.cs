5766e7c1f4efa2f7c67c81f9b1bea3b4e516303f
Marcin Kozak <marcin.kozak.dev@gmail.com>
2023-02-27 21:36:53 +0100

    Added repository

---
+++ DieticianMVC.Infrastructure/Repositories/DieticianRepository.cs
@@ -0,0 +1,36 @@
+﻿using DieticianMVC.Domain.Model;
+
+namespace DieticianMVC.Infrastructure.Repositories
+{
+    public class DieticianRepository
+    { 
+        private readonly Context _context;
+        public DieticianRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public void DeleteDietician(int dieticianId)
+        {
+            var dietician = _context.Dieticianes.Find(dieticianId);
+            if (dietician != null)
+            {
+                _context.Dieticianes.Remove(dietician);
+                _context.SaveChanges();
+            }
+        }
+
+        public int CreateDietician(Dietician dietician)
+        {
+            _context.Dieticianes.Add(dietician);
+            _context.SaveChanges();
+            return dietician.Id;
+        }
+
+        public Dietician GetDieticianById(int dieticianId)
+        {
+            var dietician = _context.Dieticianes.FirstOrDefault(i => i.Id == dieticianId);
+            return dietician;
+        }
+    }
+}
