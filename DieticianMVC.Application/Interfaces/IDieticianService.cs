e4cabd7d38177676c8a764d573e1cd3472e54d9b
Marcin Kozak <marcin.kozak.dev@gmail.com>
2023-04-12 23:23:43 +0200

    Added interface IDieticianService

--- DieticianMVC.Application/Interfaces/IDieticianService.cs
+++ DieticianMVC.Application/Interfaces/IDieticianService.cs
@@ -1,14 +1,22 @@
 ﻿using DieticianMVC.Application.ViewModels.Dietician;
+using DieticianMVC.Domain.Model;
 
 namespace DieticianMVC.Application.Interfaces
 {
     public interface IDieticianService
     {
         int CreateDietician(NewDieticianVm dieticianVm);
-        void DeleteDietician(int dieticianId);
+        Dietician GetDieticianById(int id);
         DieticianDetailsVm GetDieticianDetails(int dieticianId);
+        void DeleteDietician(int dieticianId);
         NewDieticianVm GetDieticianForEdit(int dieticianId);
         void UpdateDietician(NewDieticianVm dieticianVm);
         void UpdateDieticianDetails(DieticianDetailsVm dieticianVm);
+        Address AddAddress(NewAddressVm addressVm);
+        Address GetAddressById(int addressId);
+        NewAddressVm GetAddressForEdit(int addressId);
+        void UpdateAddress(NewAddressVm addressVm);
+        void DeleteAddress(int addressId);
+        IQueryable<AddressForListVm> GetAddressByDietician(int id);
     }
 }
